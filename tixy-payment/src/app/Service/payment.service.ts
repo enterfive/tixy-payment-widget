ce494872bb6f40fca59b9013c09b4ad78e78db9c
deji adesoga <done2stay@gmail.com>
2020-04-07 23:23:09 +0100

    Consumed buy_ticket end-point

--- tixy-payment/src/app/Service/payment.service.ts
+++ tixy-payment/src/app/Service/payment.service.ts
@@ -22,4 +22,45 @@ export class PaymentService {
       event_id
     })
   }
+
+  buyTicket(
+    event_id: string, 
+    buyer: {
+      first_name :string,
+      last_name: string,
+      email: string,
+      amount: string,
+      number_of_tickets: string,
+      fees: string
+    },
+    attendees: [
+      {
+        first_name: string,
+        last_name: string,
+        email: string,
+        ticket_category_id: string
+      }
+    ]
+    ){
+      return this.http.post(`${environment.baseUrl}/buy_ticket`,
+      {
+        event_id,
+        buyer,
+        attendees
+      })
+  }
+
+  ticketPurchaseCallback(
+    order_id: string, 
+    transaction_reference: string, 
+    payment_platform: string
+    ){
+    return this.http.post(`${environment.baseUrl}/ticket_purchase_callback`,
+    {
+      order_id,
+      transaction_reference,
+      payment_platform
+    })
+  }
+
 }
--- tixy-payment/src/app/buy-local/buy-a-ticket/buy-a-ticket.component.ts
+++ tixy-payment/src/app/buy-local/buy-a-ticket/buy-a-ticket.component.ts
@@ -12,8 +12,8 @@ import {
   EmailValidator
 } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl, SafeUrl  } from '@angular/platform-browser';
-import { IfStmt } from '@angular/compiler';
-
+import _ from 'Lodash';
+import { Key } from 'protractor';
 @Component({
   selector: 'app-buy-a-ticket',
   templateUrl: './buy-a-ticket.component.html',
@@ -40,141 +40,181 @@ export class BuyATicketComponent implements OnInit {
   total;
   add;
   window;
+  alltotal;
   mySubscription: any;
-  
+  selectedTic;
+  number = 1000
   @Output()
   onClose: EventEmitter<boolean> = new EventEmitter();
+  payment_platform = "paystack"
+  userprofileform: FormGroup
+  ticketcategory_id;
+  obj;
   // PaystackPop;
+
+  ngOnInit() {  
+    this.activatedRoute.params.subscribe(params => {
+      this.eventId = params['id'];
+     console.log(this.eventId);
+   });
+   this.getOpenEventTicket()
+}
+
 
   constructor(
     private activatedRoute: ActivatedRoute, 
     private paymentService: PaymentService,
     private _sanitizer: DomSanitizer,
-    private router: Router
-
+    private router: Router,
+    private fb: FormBuilder
     ) {
-      this.router.routeReuseStrategy.shouldReuseRoute = function () {
-        return false;
-      };
-      this.mySubscription = this.router.events.subscribe((event) => {
-        if (event instanceof NavigationEnd) {
-          // Trick the Router into believing it's last link wasn't previously loaded
-          this.router.navigated = false;
-        }
-      });
-  
-      console.log(this.mySubscription)
+      this.userprofileform = this.fb.group({
+        buyer: this.fb.group({
+         first_name: ['', Validators.required],
+         last_name: ['', Validators.required],
+         email:['', Validators.required],
+         amount: ['21000', Validators.required],
+         number_of_tickets : ['3', Validators.required],
+         fees: ['100', Validators.required]
+         }),
+         attendees: this.fb.array([
+         ])
+       })
+      //  setTimeout( () => {
+      //   this.attendeesControl()
+      //  }, 7000)
    }
-   userprofileform = new FormGroup({
-    firstname: new FormControl('', Validators.required),
-    lastname: new FormControl('',  Validators.required),
-    email: new FormControl('',  Validators.required),
-    fname: new FormControl('',  Validators.required),
-    lname: new FormControl('',  Validators.required),
-    mail: new FormControl('',  Validators.required),
-    fnameone: new FormControl('',  Validators.required),
-    lnameone: new FormControl('',  Validators.required),
-    mailone: new FormControl('',  Validators.required)
-         
-  })
+  
+  
+   attendeesControl() {
+    const controlArray = this.userprofileform.get('attendees') as FormArray;
+    Object.keys(this.ticketDetails).forEach((i) => {
+     controlArray.push(
+       this.fb.group({
+        first_name: new FormControl({ value: '', disabled: false }),
+        last_name: new FormControl({ value: '', disabled: false }),
+        email: new FormControl({ value: '', disabled: false }),
+        ticket_category_id: this.ticketDetails[i].ticket_category_id
+      })
+     )
+   })
+   console.log(controlArray.controls)
+ }  
 
+ get f() {
+  return this.userprofileform.controls;
+}
+  
 
+  // checkValue(e) {
+  //   if (e.target.checked) {
+  //     const firstname = this.userprofileform.controls["firstname"].value;
+  //     const lastname = this.userprofileform.controls["lastname"].value;
+  //     const email = this.userprofileform.controls["email"].value;
+  //     this.userprofileform.controls["fname"].setValue(firstname);
+  //     this.userprofileform.controls["lname"].setValue(lastname);
+  //     this.userprofileform.controls["mail"].setValue(email);
+  //     this.userprofileform.controls["fnameone"].setValue(firstname);
+  //     this.userprofileform.controls["lnameone"].setValue(lastname);
+  //     this.userprofileform.controls["mailone"].setValue(email);
+  //   }
+  //   else {
+  //     this.userprofileform.controls["fname"].setValue('');
+  //     this.userprofileform.controls["lname"].setValue('');
+  //     this.userprofileform.controls["mail"].setValue('');
+  //     this.userprofileform.controls["fnameone"].setValue('');
+  //     this.userprofileform.controls["lnameone"].setValue('');
+  //     this.userprofileform.controls["mailone"].setValue('');
+  //   }
 
-  checkValue(e) {
-    if (e.target.checked) {
-      const firstname = this.userprofileform.controls["firstname"].value;
-      const lastname = this.userprofileform.controls["lastname"].value;
-      const email = this.userprofileform.controls["email"].value;
-      this.userprofileform.controls["fname"].setValue(firstname);
-      this.userprofileform.controls["lname"].setValue(lastname);
-      this.userprofileform.controls["mail"].setValue(email);
-      this.userprofileform.controls["fnameone"].setValue(firstname);
-      this.userprofileform.controls["lnameone"].setValue(lastname);
-      this.userprofileform.controls["mailone"].setValue(email);
-    }
-    else {
-      this.userprofileform.controls["fname"].setValue('');
-      this.userprofileform.controls["lname"].setValue('');
-      this.userprofileform.controls["mail"].setValue('');
-      this.userprofileform.controls["fnameone"].setValue('');
-      this.userprofileform.controls["lnameone"].setValue('');
-      this.userprofileform.controls["mailone"].setValue('');
-    }
-
-  }
-  checkValueOne(e) {
-    if (e.target.checked) {
-      const firstname = this.userprofileform.controls["firstname"].value;
-      const lastname = this.userprofileform.controls["lastname"].value;
-      const email = this.userprofileform.controls["email"].value;
-      this.userprofileform.controls["fname"].setValue(firstname);
-      this.userprofileform.controls["lname"].setValue(lastname);
-      this.userprofileform.controls["mail"].setValue(email);
+  // }
+  // checkValueOne(e) {
+  //   if (e.target.checked) {
+  //     const firstname = this.userprofileform.controls["firstname"].value;
+  //     const lastname = this.userprofileform.controls["lastname"].value;
+  //     const email = this.userprofileform.controls["email"].value;
+  //     this.userprofileform.controls["fname"].setValue(firstname);
+  //     this.userprofileform.controls["lname"].setValue(lastname);
+  //     this.userprofileform.controls["mail"].setValue(email);
       
-    }
-    else {
-      this.userprofileform.controls["fnameone"].setValue('');
-      this.userprofileform.controls["lnameone"].setValue('');
-      this.userprofileform.controls["mailone"].setValue('');
-    }
+  //   }
+  //   else {
+  //     this.userprofileform.controls["fnameone"].setValue('');
+  //     this.userprofileform.controls["lnameone"].setValue('');
+  //     this.userprofileform.controls["mailone"].setValue('');
+  //   }
 
-  }
-  checkValueTwo(e) {
-    if (e.target.checked) {
-      const firstname = this.userprofileform.controls["firstname"].value;
-      const lastname = this.userprofileform.controls["lastname"].value;
-      const email = this.userprofileform.controls["email"].value;
-      this.userprofileform.controls["fnameone"].setValue(firstname);
-      this.userprofileform.controls["lnameone"].setValue(lastname);
-      this.userprofileform.controls["mailone"].setValue(email);
+  // }
+  // checkValueTwo(e) {
+  //   if (e.target.checked) {
+  //     const firstname = this.userprofileform.controls["firstname"].value;
+  //     const lastname = this.userprofileform.controls["lastname"].value;
+  //     const email = this.userprofileform.controls["email"].value;
+  //     this.userprofileform.controls["fnameone"].setValue(firstname);
+  //     this.userprofileform.controls["lnameone"].setValue(lastname);
+  //     this.userprofileform.controls["mailone"].setValue(email);
       
-    }
-    else {
-      this.userprofileform.controls["fnameone"].setValue('');
-      this.userprofileform.controls["lnameone"].setValue('');
-      this.userprofileform.controls["mailone"].setValue('');
-    }
+  //   }
+  //   else {
+  //     this.userprofileform.controls["fnameone"].setValue('');
+  //     this.userprofileform.controls["lnameone"].setValue('');
+  //     this.userprofileform.controls["mailone"].setValue('');
+  //   }
 
-  }
+  // }
 
-  ngAfterViewInit(){
-   
-  }
 
   onSubmit(){
-    console.log(this.userprofileform.value)  
+    //Information Tab Form Function
+    this.submitInformationForm()
+// Paystack Modal Starts
+setTimeout(() => {
+ 
+  let shown = document.getElementById('paystackEmbedContainer') as HTMLElement;
+  console.log(shown ? 'shown' : 'not shown');
+    (<any>window).PaystackPop.setup({
+    key: 'pk_test_0dcfa780bc08adca265572bcfa91019ffa855390',
+    email: 'customer@email.com',
+    amount: this.alltotal * 100,
+    container: 'paystackEmbedContainer',
+    callback: function(response){
+      console.log(response.reference)
+     alert('successfully subscribed. transaction ref is ' + response.reference);
+     console.log(response.reference)
+    
+   },
+   onClose: function(){
+    alert('window closed');
+},
+ });
+ 
+
+}, 2000);
+console.log( (<any>window).PaystackPop)
+// Paystack Modal Ends
+  
+    //Toggle For Modal Tab
       this.payment = true;
       this.ticket = false;
       this.information = false;
+  }
 
-   setTimeout(() => {
-        let shown = document.getElementById('paystackEmbedContainer') as HTMLElement;
-        console.log(shown ? 'shown' : 'not shown');
-        this.window =   (<any>window).PaystackPop.setup({
-          key: 'pk_live_7445b0e87d2616a05199316003a7ae8e3227a6a5',
-          email: 'customer@email.com',
-          amount: 10000 * 200,
-          container: 'paystackEmbedContainer',
-          callback: function(response){
-           alert('successfully subscribed. transaction ref is ' + response.reference);
-           console.log(response.reference)
-         },
-         onClose: function(){
-          alert('window closed');
-      },
-       });
-    }, 2000);
+  submitInformationForm(){
+ //Expected Form Object
+ const event_id = this.eventId;
+ const buyer_obj = this.f.buyer.value
+ const attendees = this.f.attendees.value
+ console.log('eventd',event_id, 'buyer_obj', buyer_obj, 'attendees', attendees)
+ this.paymentService.buyTicket(event_id, buyer_obj, attendees).subscribe( (data:any) => {
+   console.log("data", data.orderRef)
+   let orderRef = data.orderRef
+    //Toggle For Modal Tab
+    localStorage.setItem('order_id', orderRef)
+   
+ })
 
-      
   }
   
-
-  ngOnDestroy(){
-    if (this.mySubscription) {
-      this.mySubscription.unsubscribe();
-    }
-    this.window().unsubscribe()
-  }
 
   toggleTicket(){
     this.ticket = true
@@ -184,94 +224,26 @@ export class BuyATicketComponent implements OnInit {
 
   
   toggleInformation(){
-
-    // (<any>window).PaystackPop.setup({
-     
-    //   key: 'pk_live_7445b0e87d2616a05199316003a7ae8e3227a6a5',
-    //       email: 'customer@email.com',
-    //       amount: 10000 * 200,
-    //       container: 'paystackEmbedContain',
-    //       callback: function(response){
-    //        alert('successfully subscribed. transaction ref is ' + response.reference);
-    //        console.log(response.reference)
-    //      },
-    //      onClose: function(){
-    //       alert('window closed');
-    //   },
-      
-      
-    // });
+    (<any>window).PaystackPop.isInitialized = false
     this.information = true
     this.ticket = false
     this.payment = false
-
-
-    // let id = this.eventId
-    // let url = `http://localhost:4200/buy-a-local-ticket/${id}`
-    // this.router.navigateByUrl(url);
-    
-   
   }
 
-  loadPaystackModal(){
-    console.log('*** loading paystack modal...');
-  }
-
-
-  // ngAfterInit(){
-    
-  // }
-  // togglePayment(){
-  //   this.payment = true;
-  //   this.ticket = false;
-  //   this.information = false
-  // }
-
-
-// loadPayStackModal() {
-//   let shown = document.getElementById('paystackEmbedContainer')
-//   console.log(shown ? 'shown' : 'not shown');
-//     (<any>window).PaystackPop.setup({
-//       key: 'pk_live_7445b0e87d2616a05199316003a7ae8e3227a6a5',
-//       email: 'customer@email.com',
-//       amount: 10000,
-//       container: 'paystackEmbedContainer',
-//       callback: function(response){
-//        alert('successfully subscribed. transaction ref is ' + response.reference);
-//      },
-     
-//    });
-//    }
-
-  ngOnInit() {
-
-    
-    this.sendUrl()
-    this.activatedRoute.params.subscribe(params => {
-      this.eventId = params['id'];
-     console.log(this.eventId);
-   });
-   this.getOpenEventTicket()
-
-   
-}
-
-
-
+ 
 
   getOpenEventTicket(){
     const eventId = this.eventId
     this.paymentService.getOpenTicketCategory(eventId).subscribe( (res:any) => {
       this.ticketDetails = res;
       this.total = res.map(x => x.price)
-      this.add = res.map(tag => tag.price).reduce((a, b) => a + b, 0);
+      this.add = res.map(tag => tag.counterPrice).reduce((a, b) => a + b, 0);
       console.log("total", this.total, "addprice", this.add)
       console.log(this.ticketDetails)
+      this.attendeesControl()
+
     })
   }
-
-
-
 
 
   activateClass(ticketDetail){
@@ -287,17 +259,19 @@ export class BuyATicketComponent implements OnInit {
   activateClassAdd(ticketDetail,index){
     ticketDetail.active = false;
     ticketDetail.active = !ticketDetail.active;
-    const selectedTic = this.ticketDetails[index]
-    if(selectedTic.counter) { 
-     selectedTic.counter++;
-     const newPrice = selectedTic.price * selectedTic.counter;
-     selectedTic.counterPrice = newPrice
-     this.totalAdd = selectedTic.counterPrice * selectedTic.counter
+     this.selectedTic = this.ticketDetails[index]
+    if(this.selectedTic.counter) { 
+     this.selectedTic.counter++;
+     const newPrice = this.selectedTic.price * this.selectedTic.counter;
+     this.selectedTic.counterPrice = newPrice
+     this.totalAdd = this.selectedTic.counterPrice * this.selectedTic.counter
      console.log(this.totalAdd)
     //  this.total = this.totalAdd + this.totalMinus
-     console.log("counter", selectedTic.counterPrice)
+     console.log("counter the price", this.selectedTic.counterPrice)
+    this.alltotal = Object.values(this.ticketDetails).map((x:any) => x.counterPrice).reduce((a, b) => a + b, 0)
+    console.log("tic det",this.alltotal )
     } else {
-      selectedTic.counter = 1 
+      this.selectedTic.counter = 1 
     } 
     console.log("The Ticket Cat Id", ticketDetail)
   }
@@ -312,7 +286,8 @@ export class BuyATicketComponent implements OnInit {
      selectedTic.counterPrice = newPrice
     this.totalMinus = selectedTic.counterPrice
     console.log(this.totalMinus)
-
+    this.alltotal = Object.values(this.ticketDetails).map((x:any) => x.counterPrice).reduce((a, b) => b - a, 0)
+    console.log("tic det",this.alltotal )
     console.log("counter", selectedTic.counterPrice)
     } else {
      //  selectedTic.counter = 0
@@ -321,13 +296,13 @@ export class BuyATicketComponent implements OnInit {
  }
 
 
- sendUrl() {
-  this.showIframe = true
-  let playerUrl = `https://paystack.com/assets/payment/production/inline.html?id=paystack7jWUC&amp;key=pk_live_7445b0e87d2616a05199316003a7ae8e3227a6a5&amp;email=customer%40email.com&amp;amount=10000&amp;currency=NGN&amp;container=paystackEmbedContainer&amp;metadata=%7B%22referrer%22%3A%22https%3A%2F%2Ftixy-2-0.webflow.io%2Fbuy-a-ticket-alt%22%7D&amp;mode=popup&amp;hasTLSFallback=true` // try it first, if it doesn't work use the sanitized URL
-  this.trustedDashboardUrl = this._sanitizer.bypassSecurityTrustResourceUrl(playerUrl);
-  this.iframeURL = this.trustedDashboardUrl;
-  console.log( this.iframeURL)
-}
+//  sendUrl() {
+//   this.showIframe = true
+//   let playerUrl = `https://paystack.com/assets/payment/production/inline.html?id=paystack7jWUC&amp;key=pk_live_7445b0e87d2616a05199316003a7ae8e3227a6a5&amp;email=customer%40email.com&amp;amount=10000&amp;currency=NGN&amp;container=paystackEmbedContainer&amp;metadata=%7B%22referrer%22%3A%22https%3A%2F%2Ftixy-2-0.webflow.io%2Fbuy-a-ticket-alt%22%7D&amp;mode=popup&amp;hasTLSFallback=true` // try it first, if it doesn't work use the sanitized URL
+//   this.trustedDashboardUrl = this._sanitizer.bypassSecurityTrustResourceUrl(playerUrl);
+//   this.iframeURL = this.trustedDashboardUrl;
+//   console.log( this.iframeURL)
+// }
 
 }
 
